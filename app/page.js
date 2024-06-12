38cbd4dcc9adf7cc12c0d12517cab2cca447ea88
jho951 <jho951@naver.com>
2024-06-12 23:07:34 +0900

    [FEAT] next.config 설정 중

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>
+        <i> Next.config </i> <br /> 설정
+      </h1>
+    </main>
+  );
+}
