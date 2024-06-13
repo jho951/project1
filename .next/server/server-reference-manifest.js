0971fbeb802a0eedc68a181ae73b2b9fe69308af
jho951 <jho951@naver.com>
2024-06-13 12:37:39 +0900

    [MERGE]

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"FpuoxjNBpo+8dShAh6jgRXMJZrkqP1t8+CKULarhEgc=\"\n}"
--- app/page.js
+++ app/page.js
@@ -3,9 +3,4 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>
-        <i> Next.config </i> <br /> 설정
-      </h1>
-    </main>
-  );
-}
+git
